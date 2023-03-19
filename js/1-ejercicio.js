ef08b4e2ab288152894b82985b650830f1699d07
Carlos Hecker <chvodev@gmail.com>
2023-03-19 17:19:43 -0300

    Agregue el enunciado del ejercicio 1, resolvi y testie

--- js/1-ejercicio.js
+++ js/1-ejercicio.js
@@ -1,1 +1,24 @@
+// Crear un array llamado meses y que almacene el nombre de los doce meses del año. Mostrar por pantalla en forma de lista los doce nombres del arreglo.
+
+let meses = [
+  "Enero",
+  "Ferbero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+document.write("<h1>Meses del año:</h1>");
+document.write("<ul>");
+for (let i = 0; i < meses.length; i++){
+  document.write(`<li>${meses[i]}</li>`);
+  document.write("<br>")
+}
+document.write("</ul>");
 
